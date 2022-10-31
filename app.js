9d2c26a13e1057bc590a3683a9367d5d730b9a26
pht86 <114332758+pht86@users.noreply.github.com>
2022-10-31 15:04:54 +0700

    ok

---
+++ app.js
@@ -0,0 +1,32 @@
+var myApp = angular.module('crudApp', []);
+myApp.controller('crudCtrl', function ($scope){
+    var students = [
+        {name: "Nguyễn Minh Phương", birthYear: "1986", address: "Hà Nội"},
+        {name: "Nguyễn Minh Phương", birthYear: "1986", address: "Hà Nội"},
+        {name: "Nguyễn Minh Phương", birthYear: "1986", address: "Hà Nội"},
+        {name: "Nguyễn Minh Phương", birthYear: "1986", address: "Hà Nội"}
+    ]
+    $scope.students = students;
+    $scope.addUser = function(){
+        $scope.students.push($scope.newUser);
+        $scope.newUser = {};
+        $scope.message = "Thêm mới thành công!";
+    };
+    $scope.selectUser = function(user){
+        console.log(user);
+        $scope.clickedUser = user;
+    };
+    $scope.editUser = function(){
+        $scope.message = "Sửa thành công!";
+    };
+    $scope.deleteUser = function(){
+        $scope.students.splice($scope.students.indexOf($scope.clickedUser));
+        $scope.message = "Đã xóa thành công!";
+    };
+    $scope.clearMessage = function(){
+        $scope.message = "";
+    };
+});
+
+
+
